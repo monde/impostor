dd52a42112d92c16fc003bdfec5f69dab626baf0
mikemondragon <mikemondragon@0e590326-a2cf-400c-92ef-688e692edfb6>
2007-10-25 01:13:12 +0000

    implementation of WWF7.9 impostor
    git-svn-id: svn+ssh://rubyforge.org/var/svn/impostor/trunk@19 0e590326-a2cf-400c-92ef-688e692edfb6

---
+++ lib/impostor/wwf79.rb
@@ -0,0 +1,243 @@
+require 'rubygems'
+require 'hpricot'
+require 'mechanize'
+require 'logger'
+require 'cgi'
+require 'pp'
+
+##
+# Web Wiz Forums version 8.0 of the Impostor
+#
+
+class WWW::Impostor
+  
+  class Wwf79 < WWW::Impostor
+
+    ##
+    # After initializing the parent a mechanize agent is created
+
+    def initialize(config={})
+      super(config)
+      @agent = WWW::Mechanize.new
+      @agent.user_agent_alias = user_agent
+      # jar is a yaml file
+      @agent.cookie_jar.load(cookie_jar) if cookie_jar && File.exist?(cookie_jar)
+      @message = nil
+      @loggedin = false
+    end
+
+    ##
+    # clean up the state of the library and log out
+
+    def logout
+      return unless @loggedin
+
+      @agent.cookie_jar.save_as(cookie_jar) if cookie_jar
+      save_topics
+
+      @forum = nil
+      @topic = nil
+      @message = nil
+
+      @loggedin = false
+    end
+
+=begin
+    def new_topic(forum=@forum, subject=@subject, message=@message)
+      raise PostError.new("forum not set") unless forum
+      raise PostError.new("topic name not given") unless subject
+      raise PostError.new("message not set") unless message
+
+      login
+      raise PostError.new("not logged in") unless @loggedin
+
+      uri = post_message_page
+      uri.query = "FID=#{forum}"
+
+      # get the submit form
+      begin
+        page = @agent.get(uri)
+      rescue StandardError => err
+        raise PostError.new(err)
+      end
+      form = page.form('frmMessageForm')
+
+      # set up the form and submit it
+      button = form.buttons.with.name('Submit').first
+      form.subject = subject
+      form.message = message
+      begin
+        page = @agent.submit(form, button)
+      rescue StandardError => err
+        raise PostError.new(err)
+      end
+
+      error = page.search("//table[@class='errorTable']")
+      if error
+        msgs = error.search("//td")
+
+        # throttled
+        too_many = (msgs.last.innerText =~ 
+        /You have exceeded the number of posts permitted in the time span/ rescue
+        false)
+        raise ThrottledError.new(msgs.last.innerText.gsub(/\s+/m,' ').strip) if too_many
+
+        # general error
+        had_error = (error.last.innerText =~ 
+        /Error: Message Not Posted/ rescue
+        false)
+        raise PostError.new(error.last.innerText.gsub(/\s+/m,' ').strip) if had_error
+      end
+
+      # save new topic id and topic name
+      add_subject(forum, topic, subject)
+
+      @forum=forum; @topic=topic; @subject=get_subject(forum,topic); @message=message
+      return true
+    end
+
+    ##
+    # Attempt to post to the forum
+
+    def post(forum = @forum, topic = @topic, message = @message)
+      raise PostError.new("forum not set") unless forum
+      raise PostError.new("topic not set") unless topic
+      raise PostError.new("message not set") unless message
+
+      login
+      raise PostError.new("not logged in") unless @loggedin
+
+      uri = forum_posts_page
+      uri.query = "TID=#{topic}"
+
+      # get the submit form
+      begin
+        page = @agent.get(uri)
+      rescue StandardError => err
+        raise PostError.new(err)
+      end
+      form = page.form('frmMessageForm')
+
+      # set up the form and submit it
+      button = form.buttons.with.name('Submit').first
+      form.message = message
+      begin
+        page = @agent.submit(form, button)
+      rescue StandardError => err
+        raise PostError.new(err)
+      end
+
+      error = page.search("//table[@class='errorTable']")
+      if error
+        msgs = error.search("//td")
+
+        # throttled
+        too_many = (msgs.last.innerText =~ 
+        /You have exceeded the number of posts permitted in the time span/ rescue
+        false)
+        raise ThrottledError.new(msgs.last.innerText.gsub(/\s+/m,' ').strip) if too_many
+
+        # general error
+        had_error = (error.last.innerText =~ 
+        /Error: Message Not Posted/ rescue
+        false)
+        raise PostError.new(error.last.innerText.gsub(/\s+/m,' ').strip) if had_error
+      end
+
+      @forum=forum; @topic=topic; @subject=get_subject(forum,topic); @message=message
+      return true
+    end
+=end
+
+    ##
+    # Get the new posts page for the application (specific to WWF7.9)
+  
+    def forum_posts_page
+      URI.join(app_root, @config[:forum_posts_page])
+    end
+
+    ##
+    # Get the new topic page for the application (specific to WWF7.9)
+  
+    def post_message_page
+      URI.join(app_root, @config[:post_message_page])
+    end
+  
+    ##
+    # does the work of logging into WWF 8.0
+
+    def login
+      return if @loggedin
+
+      # get the login page
+      page = fetch_login_page
+
+      # return if we are already logged in from a cookie state
+      return if logged_in?(page)
+
+      # setup the form and submit
+      form, button = login_form_and_button(page)
+      page = post_login(form, button)
+
+      # set up the rest of the state if we are logged in
+      @loggedin = logged_in?(page)
+      load_topics if @loggedin
+
+      @loggedin
+    end
+
+    def version
+      @version ||= self.class.to_s
+    end
+
+    protected
+
+    ##
+    # does the work of posting the login form
+
+    def post_login(form, button)
+      begin
+        page = @agent.submit(form, button)
+      rescue StandardError => err
+        raise LoginError.new(err)
+      end
+    end
+
+    ##
+    # returns the login form and its button from the login page
+
+    def login_form_and_button(page)
+      form = page.forms.with.name('frmLogin').first
+      raise LoginError.new("unknown login page format") unless form
+
+      button = WWW::Mechanize::Button.new('Submit', 'Forum Login')
+      form.add_button_to_query(button)
+      form['name'] = username
+      form['password'] = password
+      return form, button
+    end
+
+    ##
+    # fetches the login page
+
+    def fetch_login_page
+      begin
+        page = @agent.get(login_page)
+      rescue StandardError => err
+        raise LoginError.new(err)
+      end
+    end
+
+    ##
+    # Checks if the agent is already logged by stored cookie
+
+    def logged_in?(page)
+      mm = page.search("//a[@class='nav']")
+      return false unless mm
+      mm.each do |m|
+        return true if (m.innerText =~ /Logout \[#{username}\]/ rescue false)
+      end
+      false
+    end
+  end
+end
---
+++ test/test_www_impostor_wwf79.rb
@@ -0,0 +1,371 @@
+# test helper loads all the required libraries for testing
+# with fake web
+require File.dirname(__FILE__) + "/helper"
+
+require 'test/unit'
+
+class WWW::Impostor::Wwf79Test < Test::Unit::TestCase
+  include Impostor::TestHelper
+
+  def fake(config = {})
+    WWW::Impostor::FakeWwf79.new(config)
+  end
+
+  def setup
+    FakeWeb.clean_registry()
+    @good_index = 'http://localhost/wwf79/'
+    @good_login = 'http://localhost/wwf79/login_user.asp'
+    @good_reply_form = 'http://localhost/wwf79/forum_posts.asp'
+    @good_topic_form = 'http://localhost/wwf79/post_message_form.asp'
+    @good_posting = 'http://localhost/wwf79/new_post.asp?PN='
+    @good_viewtopic = 'http://localhost/wwf79/forum_posts.asp'
+  end
+
+  class WWW::Impostor::FakeWwf79 < WWW::Impostor::Wwf79
+    def fake_loggedin=(loggedin)
+      @loggedin = loggedin
+    end
+
+    def test_fetch_login_page
+        fetch_login_page
+    end
+  end
+
+  def test_version
+    im = WWW::Impostor::Wwf79.new(config(cookies=false))
+    assert im.version
+  end
+
+  def test_fetch_login_page
+    register_good_index
+    register_good_login
+    page = fake(config).test_fetch_login_page 
+    assert page
+  end
+
+  def test_bad_login_page_should_raise_exception
+    FakeWeb.register_uri(@good_login, :method => :get, 
+                         :response => response("not found",404))
+
+    im = WWW::Impostor::Wwf79.new(config(cookies=false))
+
+    assert_raises(WWW::Impostor::LoginError) do
+      im.login
+    end
+  end
+
+  def test_bad_login_post_should_raise_exception
+    register_good_index
+    FakeWeb.register_uri(@good_login, :method => :get, 
+                         :response => response(load_page('wwf79-login.html')))
+    FakeWeb.register_uri(@good_login, :method => :post, 
+                         :response => response("not found",404))
+
+    im = WWW::Impostor::Wwf79.new(config(cookies=false))
+
+    assert_raises(WWW::Impostor::LoginError) do
+      im.login
+    end
+  end
+
+  def test_should_login
+    register_good_index
+    register_good_login
+    im = WWW::Impostor::Wwf79.new(config(cookies=false))
+    assert_equal true, im.login
+    im.logout
+  end
+
+=begin
+  def test_posting_without_forum_set_should_raise_exception
+    setup_good_fake_web
+    im = fake(config)
+    im.fake_loggedin = true
+
+    im.forum = nil
+    im.topic = 2
+    im.message = "hello ruby"
+    # topic not set so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post
+    end
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post(f=nil,t=2,m="hello ruby")
+    end
+  end
+
+  def test_posting_without_topic_set_should_raise_exception
+    setup_good_fake_web
+    im = fake(config)
+    im.fake_loggedin = true
+
+    im.forum = 2
+    im.topic = nil
+    im.message = "hello ruby"
+    # topic not set so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post
+    end
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post(f=2,t=nil,m="hello ruby")
+    end
+  end
+
+  def test_posting_without_message_set_should_raise_exception
+    setup_good_fake_web
+    im = fake(config)
+
+    im.forum = 2
+    im.topic = 2
+    im.message = nil
+    # message is not set so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post
+    end
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post(f=2,t=2,m=nil)
+    end
+  end
+
+  def test_posting_not_logged_in_should_raise_exception
+    setup_good_fake_web
+    FakeWeb.register_uri(@good_login + '?FID=0', :method => :post, 
+      :response => response(load_page('wwf79-not-logged-in.html')))
+    im = fake(config)
+
+    im.forum = 2
+    im.topic = 2
+    im.message = "hello ruby"
+    # not logged in so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post
+    end
+  end
+
+  def test_getting_bad_posting_for_post_page_should_raise_exception
+    setup_good_fake_web
+
+    FakeWeb.register_uri(@good_reply_form + '?TID=2', :method => :get, 
+                         :response => response("not found",404))
+
+    im = fake(config)
+
+    im.forum = 2
+    im.topic = 2
+    im.message = "hello ruby"
+    # bad posting page should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post
+    end
+  end
+
+  def test_bad_submit_for_post_response_should_raise_exception
+    setup_good_fake_web
+
+    FakeWeb.register_uri(@good_reply_form + '?TID=2', :method => :get, 
+                         :response => response("not found",404))
+
+    im = fake(config)
+
+    # bad submit should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.post(f=2,t=2,m="hello ruby")
+    end
+  end
+
+  def test_too_many_posts_for_post_should_raise_exception
+    setup_good_fake_web
+
+    FakeWeb.register_uri(@good_posting, :method => :post, 
+      :response => response(load_page('wwf79-too-many-posts.html')))
+
+    im = fake(config)
+
+    im.forum = 2
+    im.topic = 2
+    im.message = "hello ruby"
+    # bad posting page should throw an exception
+    assert_raises(WWW::Impostor::ThrottledError) do
+      assert im.post
+    end
+  end
+
+  def test_should_post
+    setup_good_fake_web
+
+    im = fake(config)
+    im.fake_loggedin = true
+
+    im.forum = 2
+    im.topic = 2
+    im.message = "#{Time.now} Hello there #{Time.now}"
+    assert im.post
+  end
+
+  def test_new_topic_without_forum_set_should_raise_exception
+    setup_good_fake_web
+    im = fake(config)
+    im.fake_loggedin = true
+
+    im.forum = nil
+    im.subject = "hello world"
+    im.message = "hello ruby"
+    # topic not set so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic
+    end
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic(f=nil,s="hello world",m="hello world")
+    end
+  end
+
+  def test_new_topic_without_subject_set_should_raise_exception
+    setup_good_fake_web
+    im = fake(config)
+    im.fake_loggedin = true
+
+    im.forum = 2
+    im.subject = nil
+    im.message = "hello ruby"
+    # topic not set so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic
+    end
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic(f=1,s=nil,m="hello world")
+    end
+  end
+
+  def test_new_topic_without_message_set_should_raise_exception
+    setup_good_fake_web
+    im = fake(config)
+
+    im.forum = 2
+    im.subject = "hello world"
+    im.message = nil
+
+    # message is not set so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic
+    end
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic(f=1,s="hello world",m=nil)
+    end
+  end
+
+  def test_new_topic_not_logged_in_should_raise_exception
+    setup_good_fake_web
+    FakeWeb.register_uri(@good_login, :method => :post, 
+      :response => response(load_page('wwf79-not-logged-in.html')))
+    im = fake(config)
+    # not logged in so posting should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic(f=2,s="hello world",m="hello ruby")
+    end
+  end
+
+  def test_getting_bad_posting_for_new_topic_page_should_raise_exception
+    setup_good_fake_web
+
+    FakeWeb.register_uri(@good_reply_form + 'mode=newtopic&f=2', :method => :get, 
+                         :response => response("not found",404))
+
+    im = fake(config)
+
+    # bad posting page should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic(f=2,s="hello world",m="hello ruby")
+    end
+  end
+
+  def test_bad_submit_response_for_new_topic_should_raise_exception
+    setup_good_fake_web :new_topic
+
+    FakeWeb.register_uri(@good_posting, :method => :post, 
+                         :response => response("not found",404))
+
+    im = fake(config)
+
+    # bad submit response should throw an exception
+    assert_raises(WWW::Impostor::PostError) do
+      assert im.new_topic(f=2,s="hello world",m="hello ruby")
+    end
+  end
+
+  def test_new_topic_should_work
+    setup_good_fake_web :new_topic
+
+    im = fake(config)
+
+    # bad submit response should throw an exception
+    assert_nothing_raised(WWW::Impostor::ImpostorError) do
+      assert im.new_topic(f=2,s="hello world",m="hello ruby")
+      assert_equal 2, im.forum
+      assert_equal "hello world", im.subject
+      assert_equal "hello ruby", im.message
+    end
+  end
+=end
+
+  private
+
+  def register_good_login
+    FakeWeb.register_uri(@good_login, :method => :get, 
+                         :response => response(load_page('wwf79-login.html')))
+    FakeWeb.register_uri(@good_login + '?FID=0', :method => :post, 
+                         :response => response(load_page('wwf79-logged-in.html')))
+    FakeWeb.register_uri(@good_login, :method => :post, 
+                         :response => response(load_page('wwf79-logged-in.html')))
+  end
+
+  def register_good_index
+    FakeWeb.register_uri(@good_index, :method => :get, 
+                      :response => response(load_page('wwf79-index.html')))
+  end
+
+  def register_good_posting(type = :reply)
+    # different gets and posts for "reply" and "new_topic" mode
+    case type
+    when :reply
+      FakeWeb.register_uri(@good_reply_form + '?TID=2', :method => :get, 
+                         :response => response(load_page('wwf79-new_reply_form.html')))
+      FakeWeb.register_uri(@good_posting, :method => :post, 
+                         :response => response(load_page('wwf79-post-reply-good-response.html')))
+    when :new_topic
+      FakeWeb.register_uri(@good_topic_form + '?FID=2', :method => :get, 
+                         :response => response(load_page('wwf79-get-new_topic-form-good-response.html')))
+      FakeWeb.register_uri(@good_posting, :method => :post, 
+                         :response => response(load_page('wwf79-post-new_topic-good-response.html')))
+    else
+      raise "unknown type parameter"
+    end
+  end
+
+  def setup_good_fake_web(type = :reply)
+    register_good_index
+    register_good_login
+    register_good_posting type
+  end
+
+  def config(cookies=false, config={})
+    cookie_jar = File.join(Dir.tmpdir, 'www_impostor_wwf79_test.yml')
+    c = {:app_root => @good_index,
+      :login_page => 'login_user.asp', 
+      :forum_posts_page => 'forum_posts.asp', 
+      :post_message_page => 'post_message_form.asp', 
+      :user_agent => 'Windows IE 7', 
+      :username => 'tester',
+      :password => 'test'}.merge(config)
+
+    c[:cookie_jar] = cookie_jar if cookies
+    c
+  end
+
+  def response(body, code=200)
+    res = FakeResponse.new
+    res.code = code
+    res['Content-Type'] ||= 'text/html'
+    res.body = body
+    res
+  end
+end
