8041c77c31d284005d5d7c03f3fc2721ae56fca1
mikemondragon <mikemondragon@0e590326-a2cf-400c-92ef-688e692edfb6>
2008-01-22 20:57:24 +0000

    test new_topic
    git-svn-id: svn+ssh://rubyforge.org/var/svn/impostor/trunk@177 0e590326-a2cf-400c-92ef-688e692edfb6

--- test/test_www_impostor_wwf80.rb
+++ test/test_www_impostor_wwf80.rb
@@ -403,15 +403,14 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     assert_equal "not logged in", err.original_exception.message
   end
 
-=begin
   def test_getting_bad_post_page_for_new_topic_should_raise_exception
     @im.instance_variable_set(:@loggedin, true)
     forum = 2
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
     errmsg = "from test #{Time.now.to_s}"
     WWW::Mechanize.any_instance.expects(:get).once.with(
-      post_message_page
+      new_topic_page
     ).raises(StandardError, errmsg)
 
     err = assert_raise(WWW::Impostor::PostError) do
@@ -420,15 +419,16 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     assert_equal errmsg, err.original_exception.message
   end
 
+=begin
   def test_getting_bad_post_form_for_new_topic_should_raise_exception
     @im.instance_variable_set(:@loggedin, true)
     response = {'content-type' => 'text/html'}
     body = '<form action="post_message.asp?PN=" method="post" name="frmAddMessage"></form>'
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     forum = 2
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
-    WWW::Mechanize.any_instance.expects(:get).once.with(post_message_page).returns(page)
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
+    WWW::Mechanize.any_instance.expects(:get).once.with(new_topic_page).returns(page)
     err = assert_raise(WWW::Impostor::PostError) do
       @im.new_topic(f=forum,s="hello world",m="hello ruby")
     end
@@ -441,9 +441,9 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     body = wwf80_good_submit_new_topic_form
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     forum = 2
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
-    WWW::Mechanize.any_instance.expects(:get).once.with(post_message_page).returns(page)
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
+    WWW::Mechanize.any_instance.expects(:get).once.with(new_topic_page).returns(page)
     errmsg = "from test #{Time.now.to_s}"
     WWW::Mechanize.any_instance.expects(:submit).once.raises(StandardError, errmsg)
     err = assert_raise(WWW::Impostor::PostError) do
@@ -458,9 +458,9 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     body = wwf80_good_submit_new_topic_form
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     forum = 2
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
-    WWW::Mechanize.any_instance.expects(:get).once.with(post_message_page).returns(page)
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
+    WWW::Mechanize.any_instance.expects(:get).once.with(new_topic_page).returns(page)
     body = load_page('wwf80-too-many-topics.html').join
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     WWW::Mechanize.any_instance.expects(:submit).once.returns(page)
@@ -477,9 +477,9 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     body = wwf80_good_submit_new_topic_form
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     forum = 2
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
-    WWW::Mechanize.any_instance.expects(:get).once.with(post_message_page).returns(page)
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
+    WWW::Mechanize.any_instance.expects(:get).once.with(new_topic_page).returns(page)
     body = load_page('wwf80-general-new-topic-error.html').join
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     WWW::Mechanize.any_instance.expects(:submit).once.returns(page)
@@ -496,9 +496,9 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     body = wwf80_good_submit_new_topic_form
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     forum = 2
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
-    WWW::Mechanize.any_instance.expects(:get).once.with(post_message_page).returns(page)
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
+    WWW::Mechanize.any_instance.expects(:get).once.with(new_topic_page).returns(page)
 
     body = 'junk'
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
@@ -518,9 +518,9 @@ class WWW::Impostor::Wwf80Test < Test::Unit::TestCase
     forum = 2
     subject = "hello world"
     message = "hello ruby"
-    post_message_page = @im.post_message_page
-    post_message_page.query = "FID=#{forum}"
-    WWW::Mechanize.any_instance.expects(:get).once.with(post_message_page).returns(page)
+    new_topic_page = @im.new_topic_page
+    new_topic_page.query = "FID=#{forum}"
+    WWW::Mechanize.any_instance.expects(:get).once.with(new_topic_page).returns(page)
     body = load_page('wwf80-new-topic-forum_posts-response.html').join
     page = WWW::Mechanize::Page.new(uri=nil, response, body, code=nil, mech=nil)
     WWW::Mechanize.any_instance.expects(:submit).once.returns(page)
