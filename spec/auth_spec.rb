dee89a471ec2b4c5fa2328548a067026668c47d5
Mike Mondragon <mikemondragon@gmail.com>
2011-01-07 08:33:26 -0800

    more login spec coverage

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -2,12 +2,20 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "impostor's authorization routines" do
 
-  describe "the no-op test impostor with out implemented factory methods" do
+  describe "the no-op test impostor without implemented factory methods" do
 
     it "should login at the server only if not logged in" do
       auth = self.auth
       auth.should_receive(:logged_in?).once.and_return(true)
       auth.login.should be_true
+    end
+
+    it "should not login when factory methods are not implemented" do
+      auth = self.auth
+      lambda { auth.login }.should raise_error(
+        WWW::Impostor::MissingFactoryMethodError,
+        "Impostor error: logged_in? must be implemented (StandardError)"
+      )
     end
 
     it "should raise not implemented error when logged_in? called" do
