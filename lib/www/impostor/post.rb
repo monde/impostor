00cc7cea5739ffecc74001fa2e5f42ac5ba8d7e4
Mike Mondragon <mikemondragon@gmail.com>
2011-01-11 08:29:31 -0800

    post template methods and specs

--- lib/www/impostor/post.rb
+++ lib/www/impostor/post.rb
@@ -16,16 +16,27 @@ class WWW::Impostor::Post
   # implementation for specific forum applications
   #
   # * validate_post_input(forum, topic, message)
-  # * new_reply_uri(params)
+  # * get_reply_uri(params)
   # * get_reply_page(uri)
-  # * validate_post_form(page)
-  # * set_message(page, message)
-  # * post_message(form, button)
+  # * get_post_form(page)
+  # * set_message(form, message)
+  # * post_message(form)
   # * validate_post_result(page)
 
   def post(forum, topic, message)
-    validate_post_input(forum, topic, message)
+    self.validate_post_input(forum, topic, message)
     self.auth.login_with_raises
+    uri = self.get_reply_uri(forum, topic, message)
+    page = get_reply_page(uri)
+    form = get_post_form(page)
+    set_message(form, message)
+    page = post_message(form)
+    validate_post_result(page)
+
+    { :forum => forum,
+      :topic => topic,
+      :message => "Hello World",
+      :result => true }
   end
 
   def validate_post_input(forum, topic, message)
@@ -33,6 +44,30 @@ class WWW::Impostor::Post
     raise WWW::Impostor::PostError.new("topic not set") unless topic
     raise WWW::Impostor::PostError.new("message not set") unless message
     true
+  end
+
+  def get_reply_uri(forum, topic, message)
+    raise WWW::Impostor::MissingTemplateMethodError.new("get_reply_uri must be implemented")
+  end
+
+  def get_reply_page(uri)
+    raise WWW::Impostor::MissingTemplateMethodError.new("get_reply_page must be implemented")
+  end
+
+  def get_post_form(page)
+    raise WWW::Impostor::MissingTemplateMethodError.new("get_post_form must be implemented")
+  end
+
+  def set_message(form, message)
+    raise WWW::Impostor::MissingTemplateMethodError.new("set_message must be implemented")
+  end
+
+  def post_message(form)
+    raise WWW::Impostor::MissingTemplateMethodError.new("post_message must be implemented")
+  end
+
+  def validate_post_result(page)
+    raise WWW::Impostor::MissingTemplateMethodError.new("validate_post_result must be implemented")
   end
 
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -10,7 +10,18 @@ describe "impostor's post routines" do
       auth = self.auth
       post = self.post(config, auth)
 
+      reply_uri = mock "reply_uri"
+      reply_page = mock "reply page"
+      reply_form = mock "reply form"
+      result_page = mock "result page"
+
       auth.should_receive(:login_with_raises).once.and_return(true)
+      post.should_receive(:get_reply_uri).with(1,2,"Hello World").once.and_return(reply_uri)
+      post.should_receive(:get_reply_page).with(reply_uri).once.and_return(reply_page)
+      post.should_receive(:get_post_form).with(reply_page).once.and_return(reply_form)
+      post.should_receive(:set_message).with(reply_form, "Hello World").once
+      post.should_receive(:post_message).with(reply_form).once.and_return(result_page)
+      post.should_receive(:validate_post_result).with(result_page).once
 
       lambda {
         post.post(formum=1, topic=2, message="Hello World").should == {
@@ -41,7 +52,6 @@ describe "impostor's post routines" do
   describe "the base post template methods" do
 
     it "should have post error when forum is missing in #validate_post_input" do
-      post = self.post
       lambda { post.validate_post_input(nil, 2, "Hello World") }.should raise_error(
         WWW::Impostor::PostError,
         "Impostor error: forum not set (StandardError)"
@@ -49,7 +59,6 @@ describe "impostor's post routines" do
     end
 
     it "should have post error when topic is missing in #validate_post_input" do
-      post = self.post
       lambda { post.validate_post_input(1, nil, "Hello World") }.should raise_error(
         WWW::Impostor::PostError,
         "Impostor error: topic not set (StandardError)"
@@ -57,10 +66,51 @@ describe "impostor's post routines" do
     end
 
     it "should have post error when message is missing in #validate_post_input" do
-      post = self.post
       lambda { post.validate_post_input(1, 2, nil) }.should raise_error(
         WWW::Impostor::PostError,
         "Impostor error: message not set (StandardError)"
+      )
+    end
+
+    it "should raise not implemented error when get_reply_uri called" do
+      lambda { post.get_reply_uri(nil, nil, nil) }.should raise_error(
+        WWW::Impostor::MissingTemplateMethodError,
+        "Impostor error: get_reply_uri must be implemented (StandardError)"
+      )
+    end
+
+    it "should raise not implemented error when get_reply_page called" do
+      lambda { post.get_reply_page(nil) }.should raise_error(
+        WWW::Impostor::MissingTemplateMethodError,
+        "Impostor error: get_reply_page must be implemented (StandardError)"
+      )
+    end
+
+    it "should raise not implemented error when get_post_form called" do
+      lambda { post.get_post_form(nil) }.should raise_error(
+        WWW::Impostor::MissingTemplateMethodError,
+        "Impostor error: get_post_form must be implemented (StandardError)"
+      )
+    end
+
+    it "should raise not implemented error when set_message called" do
+      lambda { post.set_message(nil, nil) }.should raise_error(
+        WWW::Impostor::MissingTemplateMethodError,
+        "Impostor error: set_message must be implemented (StandardError)"
+      )
+    end
+
+    it "should raise not implemented error when post_message called" do
+      lambda { post.post_message(nil) }.should raise_error(
+        WWW::Impostor::MissingTemplateMethodError,
+        "Impostor error: post_message must be implemented (StandardError)"
+      )
+    end
+
+    it "should raise not implemented error when validate_post_result called" do
+      lambda { post.validate_post_result(nil) }.should raise_error(
+        WWW::Impostor::MissingTemplateMethodError,
+        "Impostor error: validate_post_result must be implemented (StandardError)"
       )
     end
 
