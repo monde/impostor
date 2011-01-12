4b9d6a4c3f573e2df92943fd98851a33ec1d7e58
Mike Mondragon <mikemondragon@gmail.com>
2011-01-12 08:11:33 -0800

    articulating specs for topic specs

--- spec/topic_spec.rb
+++ spec/topic_spec.rb
@@ -6,6 +6,26 @@ describe "impostor's topic routines" do
 
     it "should create new_topic via composed template methods" do
 
+      config = self.config
+      auth = self.auth
+      topic = self.topic(config, auth)
+
+      new_topic_uri = mock "new topic uri"
+      new_topic_page = mock "new topic page"
+      new_topic_form = mock "new topic form"
+      new_topic_result = mock "new topic result page"
+
+      auth.should_receive(:login_with_raises).once.and_return(true)
+      topic.should_receive(:get_new_topic_uri).with(1, "OMG!", "Hello World").and_return(new_topic_uri)
+
+      topic.should_receive(:get_new_topic_page).with(new_topic_uri).and_return(new_topic_page)
+      topic.should_receive(:get_new_topic_form).with(new_topic_page).and_return(new_topic_form)
+      topic.should_receive(:set_subject_and_message).with(new_topic_form, "OMG!", "Hello World")
+      topic.should_receive(:post_new_topic).with(new_topic_form).and_return(new_topic_result)
+      topic.should_receive(:validate_new_topic_result).with(new_topic_result)
+      topic.should_receive(:get_topic_from_result).with(new_topic_result).and_return(2)
+      config.should_receive(:add_subject).with(1, 2, "OMG!")
+
       lambda {
         topic.new_topic(formum=1, subject="OMG!", message="Hello World").should == {
           :forum => 1,
