8d6ffa049e21d5475aa90ce3100ea51ce44cf6a8
Mike Mondragon <mikemondragon@gmail.com>
2011-01-28 12:33:03 -0800

    drying up wwf 79 auth specs

--- spec/wwf79_spec.rb
+++ spec/wwf79_spec.rb
@@ -4,80 +4,86 @@ describe "a Web Wiz Forum 7.9 impostor" do
 
   describe "authentication routines" do
 
+    before do
+      @auth = wwf79_auth
+
+      @login_uri = URI.parse("http://example.com/forum/login_user.asp")
+
+      @login_page = load_fixture_page(
+        "wwf79-login.html",
+        @auth.config.login_page, 200, @auth.config.agent
+      )
+
+      @logged_in_page = load_fixture_page(
+        "wwf79-logged-in.html",
+        @auth.config.app_root, 200, @auth.config.agent
+      )
+
+      @not_logged_in_page = load_fixture_page(
+        "wwf79-not-logged-in.html",
+        @auth.config.app_root, 200, @auth.config.agent
+      )
+
+      @junk_page = load_fixture_page(
+        "junk.html", @auth.config.login_page, 200, @auth.config.agent
+      )
+    end
+
     it "should logout only if not logged in" do
-      auth = wwf79_auth
-      auth.should_receive(:authenticated?).once.and_return(false)
-      auth.logout.should_not be_true
+      @auth.should_receive(:authenticated?).once.and_return(false)
+      @auth.logout.should_not be_true
     end
 
     it "should logout" do
-      auth = wwf79_auth
-      auth.config.should_receive(:save_topics).once
-      auth.config.should_receive(:save_cookie_jar).once
-      auth.instance_variable_set("@authenticated", true)
+      @auth.config.should_receive(:save_topics).once
+      @auth.config.should_receive(:save_cookie_jar).once
+      @auth.instance_variable_set("@authenticated", true)
 
-      auth.logout.should be_true
-      auth.authenticated?.should_not be_true
+      @auth.logout.should be_true
+      @auth.authenticated?.should_not be_true
     end
 
     it "should be logged_in? when wwf 7.9 displays the user name" do
-      auth = wwf79_auth
-      page = load_fixture_page("wwf79-logged-in.html", auth.config.app_root, 200, auth.config.agent)
-
       lambda {
-        auth.logged_in?(page).should be_true
+        @auth.logged_in?(@logged_in_page).should be_true
       }.should_not raise_error
     end
 
     it "should not be logged_in? when wwf 7.9 does not display the user name" do
-      auth = wwf79_auth
-      page = load_fixture_page("wwf79-not-logged-in.html", auth.config.app_root, 200, auth.config.agent)
-
       lambda {
-        auth.logged_in?(page).should_not be_true
+        @auth.logged_in?(@not_logged_in_page).should_not be_true
       }.should_not raise_error
     end
 
     it "should return a page from fetch_login_page" do
-      auth = wwf79_auth
-      login_uri = URI.parse("http://example.com/forum/login_user.asp")
-      auth.config.agent.should_receive(:get).with(login_uri)
+      @auth.config.agent.should_receive(:get).with(@login_uri)
 
       lambda {
-        auth.fetch_login_page
+        @auth.fetch_login_page
       }.should_not raise_error
     end
 
     it "should handle an error in fetch_login_page" do
-      auth = wwf79_auth
-      login_uri = URI.parse("http://example.com/forum/login_user.asp")
-      auth.config.agent.should_receive(:get).with(login_uri).and_raise(StandardError)
+      @auth.config.agent.should_receive(:get).with(@login_uri).and_raise(StandardError)
 
       lambda {
-        auth.fetch_login_page
+        @auth.fetch_login_page
       }.should raise_error( Impostor::LoginError )
     end
 
     it "should return a login form from get_login_form" do
-      auth = wwf79_auth
-      page = load_fixture_page("wwf79-login.html", auth.config.login_page, 200, auth.config.agent)
-
       lambda {
-        auth.get_login_form(page).name.should == 'frmLogin'
+        @auth.get_login_form(@login_page).name.should == 'frmLogin'
       }.should_not raise_error
     end
 
     it "should raise login error when get_login_form receives a bad page" do
-      auth = wwf79_auth
-      page = load_fixture_page("junk.html", auth.config.login_page, 200, auth.config.agent)
-
       lambda {
-        auth.get_login_form(page)
+        @auth.get_login_form(@junk_page)
       }.should raise_error( Impostor::LoginError )
     end
 
     it "should setup login form in set_username_and_password" do
-      auth = wwf79_auth
       form = mock "login form"
       button = mock "submit button"
       Mechanize::Form::Button.should_receive(:new).and_return(button)
@@ -85,43 +91,32 @@ describe "a Web Wiz Forum 7.9 impostor" do
       form.should_receive(:[]=).with("password", "pass")
       form.should_receive(:add_button_to_query).with(button)
       lambda {
-        auth.set_username_and_password(form).should == form
+        @auth.set_username_and_password(form).should == form
       }.should_not raise_error
     end
 
     it "should return a logged in page when posting the login" do
-      auth = wwf79_auth
-      login_page = load_fixture_page("wwf79-login.html", auth.config.login_page, 200, auth.config.agent)
-      form = auth.get_login_form(login_page)
-      logged_in_page = load_fixture_page("wwf79-logged-in.html", auth.config.app_root, 200, auth.config.agent)
-      auth.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_return(logged_in_page)
+      form = @auth.get_login_form(@login_page)
+      @auth.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_return(@logged_in_page)
       lambda {
-        auth.post_login(form).should == logged_in_page
+        @auth.post_login(form).should == @logged_in_page
       }.should_not raise_error
     end
 
     it "should raise a login error when posting has an underlying exception" do
-      auth = wwf79_auth
-      login_page = load_fixture_page("wwf79-login.html", auth.config.login_page, 200, auth.config.agent)
-      form = login_page.form('frmLogin')
-      logged_in_page = load_fixture_page("wwf79-logged-in.html", auth.config.app_root, 200, auth.config.agent)
-      auth.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_raise(StandardError)
+      form = @login_page.form('frmLogin')
+      @auth.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_raise(StandardError)
       lambda {
-        auth.post_login(form)
+        @auth.post_login(form)
       }.should raise_error( Impostor::LoginError )
     end
 
     it "should login" do
-      auth = wwf79_auth
-      login_page = load_fixture_page("wwf79-login.html", auth.config.login_page, 200, auth.config.agent)
-      logged_in_page = load_fixture_page("wwf79-logged-in.html", auth.config.app_root, 200, auth.config.agent)
-      login_uri = URI.parse("http://example.com/forum/login_user.asp")
-
-      auth.config.agent.should_receive(:get).with(login_uri).and_return(login_page)
-      auth.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_return(logged_in_page)
+      @auth.config.agent.should_receive(:get).with(@login_uri).and_return(@login_page)
+      @auth.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_return(@logged_in_page)
 
       lambda {
-        auth.login.should be_true
+        @auth.login.should be_true
       }.should_not raise_error
     end
   end
@@ -234,5 +229,119 @@ describe "a Web Wiz Forum 7.9 impostor" do
 
   end
 
-end
+  describe "topic routines" do
 
+    before do
+      @topic = wwf79_topic
+    end
+
+    it "should return new topic uri when get_new_topic_uri called" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+      lambda {
+        @topic.get_new_topic_uri(1, "OMG!", "Hello World").should == new_topic_uri
+      }.should_not raise_error
+    end
+
+    it "should return new topic page when get_new_topic_page called" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+
+      new_topic_page = load_fixture_page("wwf79-get-new_topic-form-good-response.html", new_topic_uri, 200, @topic.config.agent)
+
+      @topic.config.agent.should_receive(:get).with(new_topic_uri).and_return(new_topic_page)
+
+      lambda {
+        new_topic_uri = @topic.get_new_topic_uri(1, "OMG!", "Hello World")
+        @topic.get_new_topic_page(new_topic_uri)
+      }.should_not raise_error
+    end
+
+    it "should return new topic form when get_new_topic_form called" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+      new_topic_page = load_fixture_page("wwf79-get-new_topic-form-good-response.html", new_topic_uri, 200, @topic.config.agent)
+      lambda {
+        @topic.get_new_topic_form(new_topic_page).name.should == 'frmMessageForm'
+      }.should_not raise_error
+    end
+
+    it "should raise topic error when get_new_topic_form has error" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+      new_topic_page = load_fixture_page("wwf79-get-new_topic-form-good-response.html", new_topic_uri, 200, @topic.config.agent)
+      new_topic_page.should_receive(:form).with("frmMessageForm").and_return nil
+      lambda {
+        @topic.get_new_topic_form(new_topic_page)
+      }.should raise_error( Impostor::TopicError )
+    end
+
+    it "should set subject and message on a form when set_subject_and_message called" do
+      form = mock "wwf79 topic form"
+      form.should_receive(:subject=).with("OMG!")
+      form.should_receive(:message=).with("Hello World")
+      lambda {
+        @topic.set_subject_and_message(form, "OMG!", "Hello World")
+      }.should_not raise_error
+    end
+
+    it "should post new topic with form when post_new_topic called" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+      new_topic_result = load_fixture_page("wwf79-post-new_topic-good-response.html", new_topic_uri, 200, @topic.config.agent)
+      @topic.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_return(new_topic_result)
+      new_topic_page = load_fixture_page("wwf79-get-new_topic-form-good-response.html", new_topic_uri, 200, @topic.config.agent)
+      new_topic_form = @topic.get_new_topic_form(new_topic_page)
+      lambda {
+        @topic.post_new_topic(new_topic_form)
+      }.should_not raise_error
+    end
+
+    it "should raise topic error when posting_new_topic has an error" do
+      form = mock "a wwf79 form"
+      form.should_receive(:submit).and_raise(StandardError)
+      lambda {
+        @topic.post_new_topic(form)
+      }.should raise_error( Impostor::TopicError )
+    end
+
+    it "should not raise topic error on valid reply validate_new_topic_result(page)" do
+      page = load_fixture_page("wwf79-post-new_topic-good-response.html", @topic.config.app_root, 200, @topic.config.agent)
+      lambda {
+        @topic.validate_new_topic_result(page).should be_true
+      }.should_not raise_error
+    end
+
+    it "should raise topic error on invalid reply validate_new_topic_result(page)" do
+      page = load_fixture_page("wwf79-general-posting-error.html", @topic.config.app_root, 200, @topic.config.agent)
+      lambda {
+        @topic.validate_new_topic_result(page)
+      }.should raise_error( Impostor::TopicError )
+    end
+
+    it "should return the created topic id from get_topic_from_result" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+      new_topic_result = load_fixture_page("wwf79-post-new_topic-good-response.html", new_topic_uri, 200, @topic.config.agent)
+      lambda {
+        @topic.get_topic_from_result(new_topic_result).should == 2
+      }.should_not raise_error
+    end
+
+    it "should create new topic" do
+      new_topic_uri = URI.parse("http://example.com/forum/new_topic_form.asp?FID=1")
+      new_topic_page = load_fixture_page("wwf79-get-new_topic-form-good-response.html", new_topic_uri, 200, @topic.config.agent)
+      new_topic_result = load_fixture_page("wwf79-post-new_topic-good-response.html", new_topic_uri, 200, @topic.config.agent)
+
+      @topic.auth.should_receive(:login_with_raises)
+      @topic.config.agent.should_receive(:get).with(new_topic_uri).and_return(new_topic_page)
+      @topic.config.agent.should_receive(:submit).with(instance_of(Mechanize::Form), nil, {}).and_return(new_topic_result)
+      @topic.config.should_receive(:add_subject).with(1, 2, "OMG!")
+
+      lambda {
+        @topic.new_topic(formum=1, subject="OMG!", message="Hello World").should == {
+          :forum => 1,
+          :topic => 2,
+          :subject => "OMG!",
+          :message => "Hello World",
+          :result => true
+        }
+      }.should_not raise_error
+    end
+  end
+
+end
